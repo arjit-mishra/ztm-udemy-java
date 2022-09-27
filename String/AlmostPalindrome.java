a2adc83162cd5e324db4aa35623daa3ccca1a970
Arjit Mishra <arjitm787@gmail.com>
2022-09-27 21:07:05 +0530

    String finished

---
+++ String/AlmostPalindrome.java
@@ -0,0 +1,55 @@
+package String;
+
+public class AlmostPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal : panama";//true
+        String s1 = "A man, a plan, z a canal : panama";//true
+        String s2 = "a";//false
+        String s3 = "aabcdd";//true
+        String s4 = "race a car";//true
+        String s5 = "";//true
+        String s6 = "ab";//true
+
+        System.out.println(s + " is almost palindrome? " + isAlmostPalindrome(processString(s)));
+        System.out.println(s1 + " is almost palindrome? " + isAlmostPalindrome(processString(s1)));
+        System.out.println(s2 + " is almost palindrome? " + isAlmostPalindrome(processString(s2)));
+        System.out.println(s3 + " is almost palindrome? " + isAlmostPalindrome(processString(s3)));
+        System.out.println(s4 + " is almost palindrome? " + isAlmostPalindrome(processString(s4)));
+        System.out.println(s5 + " is almost palindrome? " + isAlmostPalindrome(processString(s5)));
+        System.out.println(s6 + " is almost palindrome? " + isAlmostPalindrome(processString(s6)));
+
+    }
+
+
+    private static boolean isAlmostPalindrome(String s) {
+        if (s.length() < 2)
+            return true;
+
+        char ch[] = s.toCharArray();
+        int lp = 0, rp = ch.length - 1;
+
+        while (lp <= rp) {
+            if (ch[lp] != ch[rp])
+                return (validSubPalindrome(ch, lp+1, rp) || validSubPalindrome(ch, lp, rp-1) );
+//                return false;
+            lp++;
+            rp--;
+        }
+
+        return true;
+    }
+
+    private static boolean validSubPalindrome(char[] ch, int lp, int rp) {
+        while (lp <= rp) {
+            if (ch[lp] != ch[rp])
+                return false;
+            lp++;
+            rp--;
+        }
+        return true;
+    }
+
+    private static String processString(String s) {
+        return s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+    }
+}
---
+++ String/ValidPalindrome.java
@@ -0,0 +1,93 @@
+package String;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal : panama";//true
+        String s1 = "";//true
+        String s2 = "abc";//false
+        String s3 = "aabcdd";//false
+        String s4 = "aacbbcaa";//true
+
+        System.out.println(s +" is palindrome? "+ isPalindrome1(processString(s)));
+        System.out.println(s1 +" is palindrome? "+ isPalindrome1(processString(s1)));
+        System.out.println(s2 +" is palindrome? "+ isPalindrome1(processString(s2)));
+        System.out.println(s3 +" is palindrome? "+ isPalindrome1(processString(s3)));
+        System.out.println(s4 +" is palindrome? "+ isPalindrome1(processString(s4)));
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println(s +" is palindrome? "+ isPalindrome2(processString(s)));
+        System.out.println(s1 +" is palindrome? "+ isPalindrome2(processString(s1)));
+        System.out.println(s2 +" is palindrome? "+ isPalindrome2(processString(s2)));
+        System.out.println(s3 +" is palindrome? "+ isPalindrome2(processString(s3)));
+        System.out.println(s4 +" is palindrome? "+ isPalindrome2(processString(s4)));
+
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println(s +" is palindrome? "+ isPalindrome3(processString(s)));
+        System.out.println(s1 +" is palindrome? "+ isPalindrome3(processString(s1)));
+        System.out.println(s2 +" is palindrome? "+ isPalindrome3(processString(s2)));
+        System.out.println(s3 +" is palindrome? "+ isPalindrome3(processString(s3)));
+        System.out.println(s4 +" is palindrome? "+ isPalindrome3(processString(s4)));
+    }
+
+    private static boolean isPalindrome3(String s) {
+        if (s.length()<2)
+            return true;
+        String reverse = new StringBuilder(s).reverse().toString();
+
+        int lp=0;
+
+        while(lp<s.length()){
+            if (s.charAt(lp) != reverse.charAt(lp))
+                return false;
+            lp++;
+        }
+
+        return true;
+    }
+
+    private static boolean isPalindrome2(String s) {
+        if (s.length()<2)
+            return true;
+
+        char ch[] = s.toCharArray();
+        int mid = ch.length / 2;
+        int rp = mid, lp = mid;
+
+        if(ch.length%2 == 0)
+            lp = mid - 1;
+
+        while(lp>=0 && rp<ch.length) {
+            if(ch[lp] != ch[rp])
+                return false;
+            lp--;
+            rp++;
+        }
+
+        return true;
+    }
+
+    private static boolean isPalindrome1(String s){
+        if (s.length()<2)
+            return true;
+
+        char ch[] = s.toCharArray();
+        int lp = 0, rp = ch.length - 1;
+
+        while(lp<=rp) {
+            if(ch[lp] != ch[rp])
+                return false;
+            lp++;
+            rp--;
+        }
+
+        return true;
+    }
+    private static String processString(String s){
+        return s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+    }
+}
